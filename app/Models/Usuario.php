4bb02867b77697e45803bc00114e7428a94f6066
Nathan Lima <nathan.lima@grupohorizonte.com.br>
2026-02-05 14:22:21 -0300

    Criação do modelo Usuario para padronização dos modelos em PT-BR

---
+++ app/Models/Usuario.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Usuario extends Model
+{
+    use HasFactory;
+
+    protected $table = 'usuarios';
+
+    protected $fillable = [
+        'nome',
+        'email',
+        'cpf',
+        'idade',
+    ];
+
+    public function vagas()
+    {
+        return $this->belongsToMany(Vaga::class, 'candidaturas');
+    }
+}
